686d9b3206d4d1589453829fbca8838a9dd53f5a
ByoungYong Kim <github@ryubro.com>
2019-02-26 09:51:38 +0100

    Initial commit

---
+++ smoothly.py
@@ -0,0 +1,9 @@
+def smooth_curve(points, factor=0.9):
+    smoothed_points = []
+    for point in points:
+        if smoothed_points:
+            previous = smoothed_points[-1]
+            smoothed_points.append(previous * factor + point * (1 - factor))
+        else:
+            smoothed_points.append(point)
+    return smoothed_points
---
+++ utils.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    return results
